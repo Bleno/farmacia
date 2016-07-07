3c41082ec75f8453ac8fd63931ebaf0d13e14400
Bleno <blenobok@gmail.com>
2016-07-07 11:34:51 -0300

    removendo arquivo do projeto antigo desnecessário

--- application/models/SexoModel.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class SexoModel extends CI_Model{
-	public function getAllSexo(){
-		$this -> db -> from('TbSexo');
-		$this -> db -> order_by('idSexo');
-		return $this -> db -> get();
-	}
-}
