4f3939cac1b3517a780dabcd06e930bb1fa48647
Bleno <blenobok@gmail.com>
2016-07-11 11:48:33 -0300

    INsert estava faltando o campo ativo do usuário

--- Banco/INSERT.sql
+++ Banco/INSERT.sql
@@ -1,2 +1,5 @@
 use farmacia_db;
-INSERT INTO `tb_usuario` (`nome`, `email`, `senha`, `dt_cadastro`,`dt_update`) VALUES ("admin", "admin@gmail.com", "123", "2016-07-01 05:50:18", "2016-07-01 05:50:18");
+INSERT INTO
+ `tb_usuario` (`nome`, `email`, `senha`, `ativo`, `dt_cadastro`,`dt_update`)
+VALUES
+ ("admin", "admin@gmail.com", "123", 1 , "2016-07-01 05:50:18", "2016-07-01 05:50:18");
