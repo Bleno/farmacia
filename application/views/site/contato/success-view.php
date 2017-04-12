aa0d8d13572b22f71cf1bcdb849bfc4287fd6801
Bleno <blenobok@gmail.com>
2017-04-12 15:19:53 -0300

    Add view

---
+++ application/views/site/contato/success-view.php
@@ -0,0 +1,17 @@
+<div class="container">
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+	<div class="section">
+		
+	</div>
+	<div class="section">
+		
+	</div>
+	<div class="col s12">
+		 <div class="card-panel teal lighten-3"><p style="color:#00796b;"><b><?php echo $nome ?></b> sua receita foi enviada com sucesso!<i class="material-icons small green-text">done</i></p></div>
+	</div>
+
+</div>
